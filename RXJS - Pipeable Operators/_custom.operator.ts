337eb4f9e0558d53f2cfc10e3ed0febb2c0a108b
Sanchit Gupta <sanchit.gupta@kohls.com>
2018-08-27 22:39:07 +0530

    Added template for generating RXJS pipeable operators

---
+++ RXJS - Pipeable Operators/_custom.operator.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+
+{{#each (__toArray operatorNames)}}
+const {{camelCase this}} = () => <T>(source: Observable<T>) =>
+    new Observable<T>(observer => {
+    return source.subscribe({
+        next(x) {
+        observer.next(x);
+        },
+        error(err) {
+        observer.error(err);
+        },
+        complete() {
+        observer.complete();
+        }
+    })
+    });
+
+{{/each}}
